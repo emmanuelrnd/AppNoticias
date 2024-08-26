de5a0a48292af97d4780d01e180b125f22e57a69
emmanuelrnd <emmanuelrnd@gmail.com>
2024-08-26 09:21:55 -0300

    "ultima prueba"

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export {AuthContext} from "./contexts/AuthContext";
+export {AuthProvider} from "./contexts/AuthContext";
+export {useAuth} from "./contexts/AuthContext";
+export * from "./hooks/useFetch"
+
+
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/appnoticias/",
-})
+  build:{
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "app noticias",
+      fileName: (format)=> `appnoticias.${format}.js`,
+    },
+    rollupOptions:{
+      external:["react", "react-dom"],
+      output:{
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        }
+      }
+    }
+  },
+});
 
